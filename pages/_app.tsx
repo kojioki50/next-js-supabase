d5b1cc65168810e589fe583b8be2ae6f00e35939
kojioki50 <kojioki50@gmail.com>
2022-04-28 16:48:49 +0900

    second commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,16 @@ import type { AppProps, NextWebVitalsMetric } from 'next/app'
 export function reportWebVitals(metric: NextWebVitalsMetric) {
   switch (metric.name) {
     case 'FCP':
-      console.log(`FCP: $(Math.round(metric.value * 10) / 10}`)
+      console.log(`FCP: ${Math.round(metric.value * 10) / 10}`)
       break
     case 'LCP':
-      console.log(`FCP: $(Math.round(metric.value * 10) / 10}`)
+      console.log(`FCP: ${Math.round(metric.value * 10) / 10}`)
       break
     case 'TTFB':
-      console.log(`FCP: $(Math.round(metric.value * 10) / 10}`)
+      console.log(`FCP: ${Math.round(metric.value * 10) / 10}`)
       break
     case 'Next.js-hydration':
-      console.log(`Hydration: $(Math.round(metric.value * 10) / 10}
+      console.log(`Hydration: ${Math.round(metric.value * 10) / 10}
       -> ${Math.round((metric.startTime + metric.value) * 10) / 10}`)
       break
      default:
